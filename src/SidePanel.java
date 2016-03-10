70e09eb2bc4234f4cb1b771c4137c75510b6edd6
Sergio Diaz <sdiazn19@gmail.com>
2016-03-10 16:58:18 -0600

    Side Panel 25 lineas todo

--- src/SidePanel.java
+++ src/SidePanel.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -8,9 +7,6 @@ import java.awt.Toolkit;
 import java.net.URL;
 
 import javax.swing.JPanel;
-
-
-  
 
 /**
  * The {@code SidePanel} class is responsible for displaying statistics and
@@ -55,7 +51,8 @@ public class SidePanel extends JPanel {
     public SidePanel(SnakeGame game) {
         this.game = game;
 
-        setPreferredSize(new Dimension(300, BoardPanel.ROW_COUNT * BoardPanel.TILE_SIZE));
+        setPreferredSize(new Dimension(300, BoardPanel.ROW_COUNT 
+                * BoardPanel.TILE_SIZE));
         setBackground(Color.WHITE);
     }
 
@@ -75,9 +72,13 @@ public class SidePanel extends JPanel {
      * Images of fruits and bad fruits
      */
     public Image imaApple;
+    
     public Image imaCucumber;
+    
     public Image imaKiwi;
+    
     public Image imaBlueBerry;
+    
     public Image imaOrange;
     
     
@@ -96,7 +97,8 @@ public class SidePanel extends JPanel {
          * Draw the game name onto the window.
          */
         g.setFont(LARGE_FONT);
-        g.drawString("Snake Game", getWidth() / 2 - g.getFontMetrics().stringWidth("Snake Game") / 2, 30);
+        g.drawString("Snake Game", getWidth() 
+                / 2 - g.getFontMetrics().stringWidth("Snake Game") / 2, 30);
 
         /**
          * Draw the categories onto the window.
@@ -116,90 +118,101 @@ public class SidePanel extends JPanel {
         
         //g.setColor(Color.DARK_GRAY);
         g.setColor(new Color(153,51,51));
-        //Draw the content for the statistics category.
-        int drawY = STATISTICS_OFFSET;
-        g.drawString("Total Score: " + game.getScore(), LARGE_OFFSET, drawY += MESSAGE_STRIDE);
-        g.drawString("Fruit Eaten: " + game.getFruitsEaten(), LARGE_OFFSET, drawY += MESSAGE_STRIDE);
-        g.drawString("Fruit Score: " + game.getNextFruitScore(), LARGE_OFFSET, drawY += MESSAGE_STRIDE);
 
-        //g.drawString("Controls", SMALL_OFFSET, drawY += MESSAGE_STRIDE);
-        //g.drawString("drawY"+drawY, SMALL_OFFSET, drawY += MESSAGE_STRIDE);
+       /**
+        * Draw fruit info.
+        */
+        drawFruitInfo(g);
         
-        
-        g.setColor(new Color(255, 51, 51).darker());
-        g.drawImage(imaApple,LARGE_OFFSET, drawY += 35, null);
-      
-        g.setColor(new Color(153,51,51));
-        g.drawString("Apple: Adds 1 ",LARGE_OFFSET+20, drawY + 12);
-        
-        g.drawImage(imaKiwi,LARGE_OFFSET, drawY += 20, null);
-        g.drawString("Kiwi: Adds 2 ",LARGE_OFFSET+20, drawY + 12);
-       
-        g.drawImage(imaBlueBerry, LARGE_OFFSET, drawY += 20, null);
-        g.drawString("Blueberry: Add 3 ",LARGE_OFFSET+20, drawY + 12);
-        
-        g.drawImage(imaCucumber, LARGE_OFFSET, drawY += 20, null);
-        g.drawString("Cucumber: Ends game inmediately",LARGE_OFFSET+20, drawY +12);
-        
-        g.drawImage(imaOrange, LARGE_OFFSET, drawY += 20, null);
-        g.drawString("Orange: Reset socre to 0",LARGE_OFFSET+20, drawY +12);
-        
-
+        /**
+         * Draw controls.
+         */
         drawControls(g);
         
     }
     
     
     /**
-         * Initializes the images. 
-         * Fruit 1 = apple
-         * Fruit 2 = kiwi
-         * Fruit 3 = blueberry
-         * Bad Fruit = cucumber
-         * Zero Fruit = orange
-         *
-         */
-        public void imageInit() {
+     * Initializes the images. Fruit 1 = apple Fruit 2 = kiwi Fruit 3 =
+     * blueberry Bad Fruit = cucumber Zero Fruit = orange
+     *
+     */
+    public void imageInit() {
 
-            URL urlImagenCucumber = this.getClass()
-                    .getResource("images/cucumber.gif");
-            imaCucumber = Toolkit.getDefaultToolkit().getImage(urlImagenCucumber);
-            
-            URL urlImagenKiwi = this.getClass()
-                    .getResource("images/kiwi.gif");
-            imaKiwi = Toolkit.getDefaultToolkit().getImage(urlImagenKiwi);
-            
-            URL urlImagenApple = this.getClass()
-                    .getResource("images/apple.png");
-            imaApple = Toolkit.getDefaultToolkit().getImage(urlImagenApple);
-            
-            URL urlImagenOrange = this.getClass()
-                    .getResource("images/orange.gif");
-            imaOrange = Toolkit.getDefaultToolkit().getImage(urlImagenOrange);
-            
-            URL urlImagenBlueBerry = this.getClass()
-                    .getResource("images/blueberry.gif");
-            imaBlueBerry = Toolkit.getDefaultToolkit().getImage(urlImagenBlueBerry);
-           
-        }
+        URL urlImagenCucumber = this.getClass()
+                .getResource("images/cucumber.gif");
+        imaCucumber = Toolkit.getDefaultToolkit().getImage(urlImagenCucumber);
+
+        URL urlImagenKiwi = this.getClass()
+                .getResource("images/kiwi.gif");
+        imaKiwi = Toolkit.getDefaultToolkit().getImage(urlImagenKiwi);
+
+        URL urlImagenApple = this.getClass()
+                .getResource("images/apple.png");
+        imaApple = Toolkit.getDefaultToolkit().getImage(urlImagenApple);
+
+        URL urlImagenOrange = this.getClass()
+                .getResource("images/orange.gif");
+        imaOrange = Toolkit.getDefaultToolkit().getImage(urlImagenOrange);
+
+        URL urlImagenBlueBerry = this.getClass()
+                .getResource("images/blueberry.gif");
+        imaBlueBerry = Toolkit.getDefaultToolkit().getImage(urlImagenBlueBerry);
+
+    }
         
         
-       /**
-        * Draw panel controls.
-        * 
-        */
-        public void drawControls(Graphics g) {
-            
-         g.setColor(new Color(153,51,51));
+    /**
+     * Draw panel controls.
+     *
+     */
+    public void drawControls(Graphics g) {
+
+        g.setColor(new Color(153, 51, 51));
         //Draw the content for the controls category.
         drawY = CONTROLS_OFFSET;
-        g.drawString("Move Up: W / Up Arrowkey", LARGE_OFFSET, drawY += MESSAGE_STRIDE);
-        g.drawString("Move Down: S / Down Arrowkey", LARGE_OFFSET, drawY += MESSAGE_STRIDE);
-        g.drawString("Move Left: A / Left Arrowkey", LARGE_OFFSET, drawY += MESSAGE_STRIDE);
-        g.drawString("Move Right: D / Right Arrowkey", LARGE_OFFSET, drawY += MESSAGE_STRIDE);
+        g.drawString("Move Up: W / Up Arrowkey", LARGE_OFFSET, drawY 
+                += MESSAGE_STRIDE);
+        g.drawString("Move Down: S / Down Arrowkey", LARGE_OFFSET, drawY 
+                += MESSAGE_STRIDE);
+        g.drawString("Move Left: A / Left Arrowkey", LARGE_OFFSET, drawY 
+                += MESSAGE_STRIDE);
+        g.drawString("Move Right: D / Right Arrowkey", LARGE_OFFSET, drawY 
+                += MESSAGE_STRIDE);
         g.drawString("Pause Game: P", LARGE_OFFSET, drawY += MESSAGE_STRIDE);
         g.drawString("Load Game: C", LARGE_OFFSET, drawY += MESSAGE_STRIDE);
         g.drawString("Save Game: G", LARGE_OFFSET, drawY += MESSAGE_STRIDE);
-        }
+
+    }
+    
+    /**
+     * Draw fruit info.
+     *
+     */
+    public void drawFruitInfo(Graphics g) {
+
+        //Draw the content for the statistics category.
+        int drawY = STATISTICS_OFFSET;
+        g.drawString("Total Score: " + game.getScore(), LARGE_OFFSET, drawY 
+                += MESSAGE_STRIDE);
+        g.drawString("Fruit Eaten: " + game.getFruitsEaten(), LARGE_OFFSET, 
+                drawY += MESSAGE_STRIDE);
+        g.drawString("Fruit Score: " + game.getNextFruitScore(), LARGE_OFFSET, 
+                drawY += MESSAGE_STRIDE);
+
+        g.setColor(new Color(255, 51, 51).darker());
+        g.drawImage(imaApple,LARGE_OFFSET, drawY += 35, null);
+        g.setColor(new Color(153,51,51));
+        g.drawString("Apple: Adds 1 ",LARGE_OFFSET+20, drawY + 12);
+        g.drawImage(imaKiwi,LARGE_OFFSET, drawY += 20, null);
+        g.drawString("Kiwi: Adds 2 ",LARGE_OFFSET+20, drawY + 12);
+        g.drawImage(imaBlueBerry, LARGE_OFFSET, drawY += 20, null);
+        g.drawString("Blueberry: Add 3 ",LARGE_OFFSET+20, drawY + 12);
+        g.drawImage(imaCucumber, LARGE_OFFSET, drawY += 20, null);
+        g.drawString("Cucumber: Ends game inmediately",LARGE_OFFSET+20, 
+                drawY + 12);
+        g.drawImage(imaOrange, LARGE_OFFSET, drawY += 20, null);
+        g.drawString("Orange: Reset socre to 0",LARGE_OFFSET+20, drawY +12);
+    }
 
 }
